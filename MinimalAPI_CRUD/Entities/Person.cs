847945f0af493eaf195f81c4694933a31928b112
Fernando Flores <luiisflores34@gmail.com>
2026-03-19 23:46:34 -0600

    Entity creation and dependency installation

---
+++ MinimalAPI_CRUD/Entities/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalAPI_CRUD.Entities
+{
+    public class Person
+    {
+        public int IDPerson { get; set; }
+        public required string Name { get; set; }
+        public required string Last_name { get; set; }
+        public required string CURP { get; set; }
+    }
+}
